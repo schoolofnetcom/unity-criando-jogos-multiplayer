991e03c800fd053650a383bfd3e32203c233f085
victor <Victor Lima>
2017-11-03 09:43:42 -0300

    Okay

---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+	void OnTriggerEnter(Collider Jogador){
+		Jogador.gameObject.transform.position = Vector3.zero;
+		Jogador.gameObject.transform.rotation = Quaternion.identity;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	private float horizontal, vertical;
+	private GameObject caps;
+	public GameObject bala;
+	public Transform ponto;
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer){
+			return;
+		}
+		Movimentacao();
+		CmdAtirar();
+	}
+
+	void Movimentacao(){
+		horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 120.0f;
+		vertical = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		transform.Rotate(0, horizontal, 0 );
+		transform.Translate(0,0,vertical);
+	}
+
+	[Command]
+	void CmdAtirar(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GameObject balaTeste = (GameObject)Instantiate(bala, ponto.position, ponto.rotation);
+			balaTeste.GetComponent<Rigidbody>().velocity = balaTeste.transform.forward * 10;
+			NetworkServer.Spawn(balaTeste);
+			Destroy(balaTeste, 3.0f);
+		}
+	}
+
+	public override void OnStartLocalPlayer(){
+		caps = transform.Find("Capsule").gameObject;
+		caps.GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+}
